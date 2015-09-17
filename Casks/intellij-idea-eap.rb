e93c1b463023bdf25c8ed5af54966ded6eb7d5d9
Chris Narburgh <Chris.Narburgh@imc-chicago.com>
2015-09-17 17:07:06 -0500

    Update IntelliJ IDEA EAP to 142.4859.6

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'intellij-idea-eap' do
-  version '142.4083.2'
-  sha256 '0455e1abaaf6fae1459d34e381394d0aac2089cf800343cdd221ed9c99618097'
+  version '142.4859.6'
+  sha256 '89f70926df65c6e3831a9b0a9b948a963d6d104f9b6a9894ecf145ec483bd953'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}-custom-jdk-bundled.dmg"
   name 'IntelliJ IDEA EAP'
